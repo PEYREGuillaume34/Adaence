65aa73821db00de450c05f864243b49cbdec14dc
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-09-08 19:44:35 +0200

    CSS Page Profils fini

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+
+// const btnAccueil = document.getElementsByClassName("accIcons");
+// const btnJeRends = document.getElementById("jerIcons");
+
+
+// if (btnAccueil) {
+//   btnAccueil.addEventListener("click", () => {
+//     console.log("Tu es sur la page d'accueil !");
+//   });
+// }
+
+
+// if (btnJeRends) {
+//   btnJeRends.addEventListener("click", () => {
+//     console.log("Tu es sur la page profil !");
+//   });
+// }
---
+++ readme.md
@@ -0,0 +1,2 @@
+Vendredi 5 Septembre : La page accueil est quasiment terminée.
+                       La page Search aussi, mais j'ai oublié la PAGINATION
